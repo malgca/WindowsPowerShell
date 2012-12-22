8cf85a43dead2d1e90b529b24af289269d06f8f8
malgca <malusi.gcakasi@gmail.com>
2012-12-22 13:55:45 +0200

    Swapped np++ for Vim as editor of choice and added commands to this effect.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,18 @@
 # Path Variables
 $SCRIPTPATH = "~\Documents\WindowsPowerShell\Scripts"
 $VIMPATH = "C:\Program Files (x86)\Vim\vim73\vim.exe"
+$GVIMPATH = "C:\Program Files (x86)\Vim\vim73\gvim.exe"
 $NETBEANSPATH = "C:\Program Files\NetBeans 7.2\bin\netbeans64.exe"
 $NPPATH = "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
 # Custom Aliases
 Set-Alias vi $VIMPATH
 Set-Alias vim $VIMPATH
+Set-Alias gvi $GVIMPATH
+Set-Alias gvim $GVIMPATH
 
 Set-Alias nb $NETBEANSPATH
 Set-Alias netbeans $NETBEANSPATH
-
-Set-Alias np++ $NPPATH
-Set-Alias notepad++ $NPPATH
 
 # Functions
 Function nav($directory) 
@@ -26,4 +26,6 @@ Function nav($directory)
 
 Function edit-profile { vim $profile }
 
-Function edit-vimrc { vim "C:\Program Files (x86)\Vim\_vimrc" }
+Function edit-vimrc { vim "~\_vimrc" }
+
+Function edit-gvimrc { gvim "~\_gvimrc" }
