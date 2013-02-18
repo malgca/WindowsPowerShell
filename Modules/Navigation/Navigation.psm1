09527492ae36d453ed6eabdce01e4761f9bc6607
Malusi Gcakasi <malusi.gcakasi@gmail.com>
2013-02-18 14:45:38 +0200

    Fixed more merge conflicts found in the Navigation and Edit modules, created new University branch for university changes

--- Modules/Navigation/Navigation.psm1
+++ Modules/Navigation/Navigation.psm1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # File containing short name aliases (and their functions) for places I navigate to often.
 
 function Move-WindowsPowerShell 
@@ -19,16 +18,6 @@ function Move-WindowsPowerShellTests
 function Move-Home
 {
 	cd $env:HOME
-}
-
-function Move-UserDir
-{
-	if($env:HOME -ne "C:\Users\$env:USERNAME\")
-	{
-		cd "C:\Users\$env:USERNAME\"
-	}
-
-	Move-Home
 }
 
 function Move-PoshGit
@@ -54,53 +43,6 @@ Set-Alias tests Move-WindowsPowerShellTests
 Set-Alias home Move-Home
 Set-Alias posh Move-PoshGit
 Set-Alias dm Move-DownloadManga
-Set-Alias userdir Move-UserDir
-# File containing short name aliases (and their functions) for places I navigate to often.
-
-function Move-WindowsPowerShell
-{
-	cd "~\Documents\WindowsPowerShell\" 
-}
-
-function Move-WindowsPowerShellModules 
-{
-	cd "~\Documents\WindowsPowerShell\Modules\"
-}
-
-function Move-WindowsPowerShellTests
-{
-	cd "~\Documents\WindowsPowerShell\Scripts\Test\"
-}
-
-function Move-Home
-{
-	cd "~\"
-}
-
-function Move-PoshGit
-{
-	cd "~\Documents\WindowsPowerShell\Modules\Posh-Git\"
-}
-
-function Move-WindowsPowerShellScripts
-{
-	cd "~\Documents\WindowsPowerShell\Scripts\"
-}
-
-function Move-DownloadManga
-{
-	cd "~\Documents\WindowsPowerShell\Scripts\MangaDownload\"
-}
-
-# Navigation aliases
-Set-Alias wps Move-WindowsPowerShell
-Set-Alias scripts Move-WindowsPowerShellScripts
-Set-Alias mods Move-WindowsPowerShellModules
-Set-Alias tests Move-WindowsPowerShellTests
-Set-Alias home Move-Home
-Set-Alias posh Move-PoshGit
-Set-Alias dm Move-DownloadManga
-Set-Alias suggestotron Move-SuggestotronApp
 
 # Export members so they can be picked up as aliases in the $profile
 # file
