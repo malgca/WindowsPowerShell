7db60a8cb6749ab56ea8354dcb043d24fcfee3d6
Malusi Gcakasi <malusi.gcakasi@gmail.com>
2013-03-22 12:36:57 +0200

    Added navigation aliases for project.

--- Modules/Navigation/Navigation.psm1
+++ Modules/Navigation/Navigation.psm1
@@ -1,4 +1,7 @@
-# File containing short name aliases (and their functions) for places I navigate to often.
+# Description: File containing short name aliases (and their functions) for places I navigate to often.
+# Author: Malusi Gcakasi
+# Version: 0.1
+# Last Modified: Mar 22, 2013 12:33 PM
 
 function Move-WindowsPowerShell 
 { 
@@ -45,16 +48,42 @@ function Move-BalancedScorecardProject
 	cd "C:\Users\$env:USERNAME\Documents\University\Balanced Scorecard Project\"
 }
 
+function Move-ProjectTreatise
+{
+	cd "C:\Users\$env:USERNAME\Documents\University\Balanced Scorecard Project\Treatise"
+}
+
+function Move-ProjectReadingList
+{
+	cd "C:\Users\$env:USERNAME\Documents\University\Balanced Scorecard Project\Reading List"
+}
+
+function Move-ProjectMeetings
+{
+	cd "C:\Users\$env:USERNAME\Documents\University\Balanced Scorecard Project\Meetings"
+}
+
+function Move-ProjectTemplates
+{
+	cd "C:\Users\$env:USERNAME\Documents\University\Balanced Scorecard Project\Templates"
+}
+
 # Navigation aliases
 Set-Alias wps Move-WindowsPowerShell
 Set-Alias scripts Move-WindowsPowerShellScripts
 Set-Alias mods Move-WindowsPowerShellModules
 Set-Alias tests Move-WindowsPowerShellTests
-Set-Alias home Move-Home
 Set-Alias posh Move-PoshGit
 Set-Alias dm Move-DownloadManga
-Set-Alias proj Move-BalancedScorecardProject
+
+Set-Alias home Move-Home
 Set-Alias vimfol Move-VimFolder
+
+Set-Alias projfol Move-BalancedScorecardProject
+Set-Alias projtre Move-ProjectTreatise
+Set-Alias projrl Move-ProjectReadingList
+Set-Alias projmee Move-ProjectMeetings
+Set-Alias projtem Move-ProjectTemplates
 
 # Export members so they can be picked up as aliases in the $profile
 # file
